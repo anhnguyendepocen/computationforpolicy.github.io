effd1eb2f018da3cd9b2ee31842ac37fc9407e0a
Eric Potash <eric@k2co3.net>
2016-02-07 12:45:39 -0600

    assignment 3 and solutions

---
+++ assignments/01_solutions.sql
@@ -0,0 +1,57 @@
+﻿-- Question 2: How often did a crime result in an arrest?
+SELECT
+	sum(arrest::int)*1.0 / count(*) AS arrest_prop 
+FROM crimes;
+
+
+
+-- Question 3: Which types of crimes most often result in arrest?
+SELECT primary_type, count(*), min(date), 
+	sum(arrest::int)*1.0 / count(*) AS arrest_prop
+FROM crimes
+GROUP BY primary_type;
+
+
+
+
+-- Question 4: What are the number of weapons violations (one of the Primary Types) per district?
+SELECT 
+	district, count(*)
+FROM crimes
+WHERE primary_type = 'WEAPONS VIOLATION'
+GROUP BY 1
+ORDER BY 2;
+
+
+
+
+-- Question 5: What are the number of arrests per days of the week? 
+-- Which day of the week has the most arrests?
+SELECT extract(dow from date) AS dayofweek, 
+	count(*) as count, sum(arrest::int) as num_arrests
+FROM crimes
+GROUP BY extract(dow from date);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assignments/02_solutions.sql
@@ -0,0 +1,51 @@
+﻿--Question 1: Crime counts and socioeconomics
+--Download the crime data for all of the year 2015. Also download the socioeconomic data.
+
+-- (a) Calculate the number of crimes in each Community Area in 2015.
+SELECT community_area, count(*)
+FROM crimes
+GROUP BY 1;
+
+
+-- (b) Sort the Community Areas by 2015 crime count. Which Community Area (by name) has the highest crime count. The lowest?
+SELECT community_area_name, count(*)
+FROM crimes
+JOIN socio ON crimes.community_area = socio.community_area_number
+GROUP BY 1
+ORDER BY 2;
+
+-- (c) Create a table whose rows are days in the year and columns are the 77 Community Area crime counts. 
+-- Select a few Communities that you are interested and plot time series.
+
+SELECT community_area, date::date, count(*)
+FROM crimes JOIN socio ON crimes.community_area = socio.community_area_number
+GROUP BY 1,2
+
+-- (d) By joining with the socioeconomic data, create a scatter plot of crime counts against per capita income. Summarize the relationship in words.
+
+
+SELECT community_area_name, count(*), avg(per_capita_income)
+FROM crimes
+JOIN socio ON crimes.community_area = socio.community_area_number
+group by 1;
+
+-- Question 2: Community Area populations
+-- Download the census block population data and the Community Area tracts mapping.
+
+-- (a) Join these together using the fact that the last six digits of the tract id in the mapping 
+-- data correspond to the first six digits of the block id. However, the data portal has a bug: if the block starts with a zero, that digit is missing!
+
+SELECT *
+FROM community_tracts c
+JOIN block_population b
+ON substring(c.tract_id::text from 6 for 6) =
+   substring(lpad(b.census_block::text, 10, '0') for 6);
+
+-- (b) Calculate the total population in each Community Area.
+
+SELECT community_id, sum(population)
+FROM community_tracts c
+JOIN block_population b
+ON substring(c.tract_id::text from 6 for 6) =
+   substring(lpad(b.census_block::text, 10, '0') for 6)
+GROUP BY 1;
